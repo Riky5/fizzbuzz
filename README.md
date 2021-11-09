5371d8b09ac41c24eceb254d0b24c80185f9c638
riky moroni <riky5.moroni@gmail.com>
2021-11-09 17:10:22 +0000

    Add README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# FizzBuzz
+
+Simple coding challenge to be completed with pair programming and TDD.
+
+## Getting started
+
+`git clone https://github.com/Riky5/fizzbuzz.git`
+
+## Usage
+
+Use `irb` 
+`require './lib/fizzbuzz.rb'`
+Pass a number of your choice
+`e.g. fizzbuzz(54687)`
+or a sequence of numbers
+`e.g, (1..100).each {|number| puts "#{number} --> #{fizzbuzz(number)}"}`
+
+## Running tests
+
+`rspec`
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,13 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when divisable by 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "Fizz" when divisable by 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end 
-  it 'returns "buzz" when divisable by 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  it 'returns "Buzz" when divisable by 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
   end
-  it 'returns "fizzbuzz" when divisable by 3 and 5' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  it 'returns "FizzBuzz" when divisable by 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
   end
   it 'returns the number when is not divisable by 3 or 5' do
     expect(fizzbuzz(4)).to eq 4
